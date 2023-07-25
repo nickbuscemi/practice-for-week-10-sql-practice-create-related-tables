f8935a9003e5c6e980eb590a50eaa9622d37cd8c
Nick <nick.buscemi13@gmail.com>
2023-07-25 15:58:15 -0400

    finished

--- sql-practice/music.sql
+++ sql-practice/music.sql
@@ -11,9 +11,19 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  band_id INTEGER,
+  FOREIGN KEY(band_id) REFERENCES bands(id)
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE musician_instruments (
+  musician_id INTEGER,
+  instrument_id INTEGER,
+  PRIMARY KEY (musician_id, instrument_id),
+  FOREIGN KEY(musician_id) REFERENCES musicians(id),
+  FOREIGN KEY(instrument_id) REFERENCES instruments(id)
 );
